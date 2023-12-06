ad5a52b4b4379b200b756ae991febfa81b130e0e
이경용 <sleekydz86@icloud.com>
2023-12-06 21:58:05 +0900

    셋팅
    셋팅

---
+++ src/main/java/com/jjsetting/jpajsporacle/JpajsporacleApplication.java
@@ -0,0 +1,13 @@
+package com.jjsetting.jpajsporacle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpajsporacleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpajsporacleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jjsetting/jpajsporacle/JpajsporacleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjsetting.jpajsporacle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpajsporacleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
