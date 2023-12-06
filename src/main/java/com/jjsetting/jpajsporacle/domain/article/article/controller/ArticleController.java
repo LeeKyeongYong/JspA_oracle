7ce72875f9a7f1e1f027c2d97a2871c513099718
이경용 <sleekydz86@naver.com>
2023-12-07 00:00:18 +0900

    오기정정
    오기정정

--- src/main/java/com/jjsetting/jpajsporacle/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/jjsetting/jpajsporacle/domain/article/article/controller/ArticleController.java
@@ -2,9 +2,10 @@ package com.jjsetting.jpajsporacle.domain.article.article.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@GetMapping("/article")
+@RequestMapping("/article")
 public class ArticleController {
     @GetMapping("/list")
     public String list(){
